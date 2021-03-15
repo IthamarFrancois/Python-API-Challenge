da9cf051f89203c22345b01a42e5a053c73d4f77
Ithamar Francois <77698337+IthamarFrancois@users.noreply.github.com>
2021-03-15 18:09:22 -0400

    Attempt 2...

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,6 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "8f818372da0128048522d56de3aaa224"
+weather_api_key = "ENTER API KEY HERE"
 
-# Temp Block 
-#3ea75e2f46705a010dc60fb322e3cfb2
 
+# https://openweathermap.org/api
